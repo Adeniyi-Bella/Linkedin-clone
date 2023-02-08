5f76d23c89abb8031ed91aba0a7251bdee1f6be1
Adeniyi-Bella <adeniyibella@gmail.com>
2023-02-08 11:23:49 +0100

    Google Authentication created with next.js

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,25 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
+import clientPromise from "../../../lib/mongodb"
+
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    })
+  ],
+  secret: process.env.JWT_SECRET,
+  adapter: MongoDBAdapter(clientPromise),
+  pages: {
+    signIn: "/home",
+  },
+  session: {
+    strategy: "jwt",
+  },
+}
+
+export default NextAuth(authOptions)

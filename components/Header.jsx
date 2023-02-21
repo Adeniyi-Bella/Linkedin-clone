a8e84c05ba9b4cf48c17f0054de25a62e429b5d5
Adeniyi-Bella <adeniyibella@gmail.com>
2023-02-21 19:13:54 +0100

    change themes based on user selection

--- components/Header.jsx
+++ components/Header.jsx
@@ -25,13 +25,22 @@ function Header() {
   // After mounting, we have access to the theme
   useEffect(() => setMounted(true), []);
 
-  //   // console.log("Current theme is", theme);
+  console.log("Current theme is", theme);
 
   return (
     <header className="sticky top-0 z-40 bg-white dark:bg-[#1D2226] flex items-center justify-around py-1.5 px-3 focus-within:shadow-lg">
       {/* Left */}
       <div className="flex items-center space-x-2 w-full max-w-xs">
-        <Image src="https://rb.gy/bizvqj" width={45} height={45} />
+        {mounted && (
+          <>
+            {resolvedTheme === "dark" ? (
+              <Image src="https://rb.gy/bizvqj" width={45} height={45} alt="" />
+            ) : (
+              <Image src="https://rb.gy/dpmd9s" width={55} height={55} alt=""  />
+            )}
+          </>
+        )}
+
         <div className="flex items-center space-x-1 dark:md:bg-gray-700 py-2.5 px-4 rounded w-full">
           <SearchRoundedIcon />
           <input
@@ -41,8 +50,8 @@ function Header() {
           />
         </div>
       </div>
-
       {/* Right */}
+
       <div className="flex items-center space-x-6">
         <HeaderLink Icon={HomeRoundedIcon} text="Home" feed active />
         <HeaderLink Icon={GroupIcon} text="My Network" feed />
@@ -74,61 +83,6 @@ function Header() {
         )}
       </div>
     </header>
-    //     <header className="sticky top-0 z-40 bg-white dark:bg-[#1D2226] flex items-center justify-around py-1.5 px-3 focus-within:shadow-lg">
-    // {/* Left */}
-    //       <div className="flex items-center space-x-2 w-full max-w-xs">
-    //         {mounted && (
-    //           <>
-    //             {resolvedTheme === "dark" ? (
-    //               <Image src="https://rb.gy/bizvqj" width={45} height={45} />
-    //             ) : (
-    //               <Image src="https://rb.gy/dpmd9s" width={55} height={55} />
-    //             )}
-    //           </>
-    //         )}
-
-    //         <div className="flex items-center space-x-1 dark:md:bg-gray-700 py-2.5 px-4 rounded w-full">
-    //           <SearchRoundedIcon />
-    //           <input
-    //             type="text"
-    //             placeholder="Search"
-    //             className="hidden md:inline-flex bg-transparent text-sm focus:outline-none placeholder-black/70 dark:placeholder-white/75 flex-grow"
-    //           />
-    //         </div>
-    // </div>
-    //       {/* Right */}
-
-    //       <div className="flex items-center space-x-6">
-    //         <HeaderLink Icon={HomeRoundedIcon} text="Home" feed active />
-    //         <HeaderLink Icon={GroupIcon} text="My Network" feed />
-    //         <HeaderLink Icon={BusinessCenterIcon} text="Jobs" feed hidden />
-    //         <HeaderLink Icon={ChatIcon} text="Messaging" feed />
-    //         <HeaderLink Icon={NotificationsIcon} text="Notifications" feed />
-    //         {/* <HeaderLink Icon={Avatar} text="Me" feed avatar hidden /> */}
-    //         <HeaderLink Icon={AppsOutlinedIcon} text="Work" feed hidden />
-
-    //         {/* Dark mode toggle */}
-    //         {mounted && (
-    //           <div
-    //             className={`bg-gray-600 flex items-center px-0.5 rounded-full h-6 w-12 cursor-pointer flex-shrink-0 relative ${
-    //               resolvedTheme === "dark" ? "justify-end" : "justify-start"
-    //             }`}
-    //             onClick={() =>
-    //               setTheme(resolvedTheme === "dark" ? "light" : "dark")
-    //             }
-    //           >
-    //             <span className="absolute left-0">🌜</span>
-    //             {/* <motion.div
-    //               className="w-5 h-5 bg-white rounded-full z-40"
-    //               layout
-    //               transition={spring}
-    //             /> */}
-
-    //             <span className="absolute right-0.5">🌞</span>
-    //           </div>
-    //         )}
-    //       </div>
-    //     </header>
   );
 }
 

b67033cf8be3ba50f8fea5cfd7fd285a1eed37c1
Adeniyi-Bella <adeniyibella@gmail.com>
2023-03-05 17:19:22 +0100

    get real time posts and load the page

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -5,33 +5,38 @@ import { handlePostState, useSSRPostsState } from "../atoms/postAtom";
 import Post from "./Post";
 
 function Feed({ posts }) {
-//   const [realtimePosts, setRealtimePosts] = useState([]);
-//   const [handlePost, setHandlePost] = useRecoilState(handlePostState);
-//   const [useSSRPosts, setUseSSRPosts] = useRecoilState(useSSRPostsState);
+  // when the user posts, the useEffect hook will run again and fetch the new posts from the database with the latest post at the top
+  const [realtimePosts, setRealtimePosts] = useState([]);
+  //useRecoilState works like useState but it's a global state
+  const [handlePost, setHandlePost] = useRecoilState(handlePostState);
+  //useSSRPostsState is a global state that is set to true by default. This is so that when the user first loads the page, the posts are rendered on the server side. This is so that the user doesn't have to wait for the posts to load. Once the user posts, the useEffect hook will run again and fetch the new posts from the database with the latest post at the top. This is when we set the useSSRPostsState to false so that the posts are rendered on the client side.
+  const [useSSRPosts, setUseSSRPosts] = useRecoilState(useSSRPostsState);
 
-//   useEffect(() => {
-//     const fetchPosts = async () => {
-//       const response = await fetch("/api/posts", {
-//         method: "GET",
-//         headers: { "Content-Type": "application/json" },
-//       });
-
-//       const responseData = await response.json();
-//       setRealtimePosts(responseData);
-//       setHandlePost(false);
-//       setUseSSRPosts(false);
-//     };
-
-//     fetchPosts();
-//   }, [handlePost]);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch("/api/posts", {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+      });
+      console.log(20);
+      const responseData = await response.json();
+      setRealtimePosts(responseData);
+      setHandlePost(false);
+      //set serverside rendering to false once the users posts
+      setUseSSRPosts(false);
+    };
+    //invoke the fetchPosts function inside useEffect
+    fetchPosts();
+    //set the handlePost to false so that the useEffect hook doesn't run again. This is because we only want to run the useEffect hook once when the user posts.
+  }, [handlePost]);
 
   return (
     <div className="space-y-6 pb-24 max-w-lg">
       <Input />
       {/* Posts */}
-      {/* {!useSSRPosts
+      {!useSSRPosts
         ? realtimePosts.map((post) => <Post key={post._id} post={post} />)
-        : posts.map((post) => <Post key={post._id} post={post} />)} */}
+        : posts.map((post) => <Post key={post._id} post={post} />)}
     </div>
   );
 }
